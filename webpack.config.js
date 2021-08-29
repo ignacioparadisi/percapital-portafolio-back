87a7191c5832a3f01804081d167f5f9fd5e26678
Ignacio Paradisi <ignacioparadisi@gmail.com>
2021-08-29 18:56:45 -0500

    Configure apollo server and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  devtool: 'source-map',
+  target: 'node',
   entry: './src/server.ts',
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
