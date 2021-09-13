7fa299495ea7d527af27dbb9af6bfae32af7a51f
Angel Rivero <angel.rivero@kavak.com>
2021-09-13 02:55:40 -0500

    added playground in landing page

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,22 @@
 import { ApolloServer } from 'apollo-server';
 import { config } from 'dotenv';
-
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import { resolvers } from '@Services/resolvers/index';
 import { typeDefs } from '@Services/schemas/index';
 
 config({ path: `${process.cwd()}/.env` });
 
 const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers
+    typeDefs: typeDefs, 
+    resolvers: resolvers,
+    introspection: true,
+    plugins: [
+       ApolloServerPluginLandingPageGraphQLPlayground(),
+    ]
 });
 
 const port = process.env.PORT || 4000
 server.listen({ port }).then(() => {
     console.info(`🚀 Server ready at ${port}`);
 });
+
