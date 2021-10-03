c21c48b45fd3850c75f719acb95f588a8b1f78f4
Ignacio Paradisi <ignacioparadisi@gmail.com>
2021-10-03 14:18:02 -0500

    Create service for creating titles

--- src/Services/resolvers/StockExchangeTitle.ts
+++ src/Services/resolvers/StockExchangeTitle.ts
@@ -8,7 +8,7 @@ export const StockExchangeTitleResolver = {
         getStockExchangeTitles: async (parent: any, args: GraphQLQuery) => {
             console.info('getStockExchangeTitles parent:', parent, 'args: ',args);
             const where = new StockExchangeTitle(args.where as StockExchangeTitle);
-            let command = StockExchangeTitleCommandFactory.createGetStockExchangeTitlesCommand(where, args.limit, args.skip);
+            const command = StockExchangeTitleCommandFactory.createGetStockExchangeTitlesCommand(where, args.limit, args.skip);
             return command.execute();
         }
     },
@@ -23,7 +23,8 @@ export const StockExchangeTitleResolver = {
         createStockExchangeTitle: async (parent: any, args: GraphQLMutation) => {
             console.info('createStockExchangeTitle parent: ', parent, 'args: ',args);
             const createData = new StockExchangeTitle(args.insertData as StockExchangeTitle);
-            return null;
+            const command = StockExchangeTitleCommandFactory.createCreateStockExchangeTitleCommand(createData);
+            return command.execute();
         },
         updateStockExchangeTitle: async (parent: any, args: GraphQLMutation) => {
             console.info('updateStockExchangeTitle parent: ', parent, 'args: ',args);
