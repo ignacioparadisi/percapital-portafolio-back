556376adf8e3cb09d51bd15562544883864ca640
Angel Rivero <angel.rivero@kavak.com>
2021-10-11 00:07:08 -0500

    renamed common functions

--- scripts/database/reset.sh
+++ scripts/database/reset.sh
@@ -2,6 +2,6 @@ psql -U percapital -tc "SELECT 1 FROM pg_database WHERE datname = 'percapital';"
 psql percapital -U percapital -f drop.sql
 psql percapital -U percapital -f create.sql
 psql percapital -U percapital -f insert.sql
-psql percapital -U percapital -f functions.sql
+psql percapital -U percapital -f common_functions.sql
 psql percapital -U percapital -f operation_functions.sql
 psql percapital -U percapital -f price_rv_functions.sql
