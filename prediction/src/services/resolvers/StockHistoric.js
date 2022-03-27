4b3a6eeda066086a723961f1622f3c9815b25fb8
Ignacio Paradisi <ignacioparadisi@gmail.com>
2022-03-27 12:59:40 -0600

    Get history for a symbol

--- prediction/src/services/resolvers/StockHistoric.js
+++ prediction/src/services/resolvers/StockHistoric.js
@@ -10,7 +10,7 @@ module.exports = {
             },
             getStockHistoricBySymbol: async (parent, args) => {
                 console.info('getStockHistoricBySymbol parent:', parent, 'args: ',args);
-                let command = StockHistoricCommandFactory.createGetStockHistoricBySymbol(args.symbol);
+                let command = StockHistoricCommandFactory.createGetStockHistoricBySymbol(args.symbol, args.interval);
                 return command.execute();
             },
             getPrediction: async (parent, args) => {
